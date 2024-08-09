157060b407f20eca300e10a43cdbde2d07f772bb
hagerezz <queen.emilly12@gmail.com>
2024-08-09 04:54:29 +0300

    hi

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const [idx, value] of array.entries()) {
-      array[idx] = appendString + value;
+    const result = [];
+    for (const idx of array) {
+      result.push(`${appendString}${idx}`);
     }
   
-    return array;
+    return result;
 }
