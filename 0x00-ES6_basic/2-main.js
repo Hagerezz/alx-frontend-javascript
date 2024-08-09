0e91cf55eb82717ef39557e0a45063398be7749f
hagerezz <queen.emilly12@gmail.com>
2024-08-09 04:03:56 +0300

    hi

--- 0x00-ES6_basic/2-main.js
+++ 0x00-ES6_basic/2-main.js
@@ -1,5 +1,3 @@
-import getSumOfHoods from './3-default-parameter.js';
+import getSanFranciscoDescription from './6-string-interpolation.js';
 
-console.log(getSumOfHoods(34));
-console.log(getSumOfHoods(34, 3));
-console.log(getSumOfHoods(34, 3, 4));
+console.log(getSanFranciscoDescription());
