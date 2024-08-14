7fd5a12dc6dd938d2ec9fe417456b6ccc2bb3b6d
hagerezz <queen.emilly12@gmail.com>
2024-08-14 05:22:26 +0300

    hi

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(array, city, newgrades) {
+  return array.filter((student) => student.location === city).map((student) => {
+    const studentGrade = newgrades.find((grade) => grade.studentId === student.id);
+    return {
+      ...array,
+      grade: studentGrade ? studentGrade.grade : 'N/A',
+    };
+  });
+}
